2344cfd0b3e65bca017fae355c7638d44dd14b6a
Javier Herrera <jaaah0010@gmail.com>
2021-11-26 10:47:00 -0600

    base

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+// Route index.js
+// Import
+const router = require('express').Router()
+
+const home = require('./../controllers')
+
+// Routes
+router.get('/', home)
+
+// Export
+module.exports = router
+
+
